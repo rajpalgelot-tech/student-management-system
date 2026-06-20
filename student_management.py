cae28918214c6d88758b93cf0342f94ec4fd71c4
rajpalgelot-tech <rajpalgelot16705@gmail.com>
2026-06-20 16:27:17 -0400

    Initial Student Management System

--- student_management.py
+++ student_management.py
@@ -1,1 +1,18 @@
+students = {}
 
+while True:
+    print("1 Add Student")
+    print("2 View Students")
+    print("3 Exit")
+
+    choice = input()
+
+    if choice == "1":
+        name = input("Name: ")
+        students[name] = True
+
+    elif choice == "2":
+        print(students)
+
+    elif choice == "3":
+        break
